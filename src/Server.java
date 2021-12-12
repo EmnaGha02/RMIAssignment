e06412da3cf2ea131ea6cec648ff37b43243c5d6
Emna Gharbi <76482966+EmnaGha02@users.noreply.github.com>
2021-12-12 23:12:38 +0100

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -9,9 +9,41 @@ public class Server extends UnicastRemoteObject implements MyInterface {
 
     public static void main(String[] args) throws RemoteException, MalformedURLException {
         Server server = new Server();
-
         java.rmi.registry.LocateRegistry.createRegistry(1250);
-
         Naming.rebind("rmi://127.0.0.1:1250/server", server);
+         }
+    
+    
+    
+    public String reverse(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+
+    
+    
+    public Character min(String s) {
+        char min = 'z';
+        for (int i = 0; i < s.length(); i++) 
+            if (s.charAt(i) <= min)
+                min = s.charAt(i);
+                return min;
+    }
+
+    
+    
+    public String caseChanger(String s) {
+        StringBuilder chaine = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
+                char res = (char) (s.charAt(i) + 32);
+                chaine.append(res);
+            } else if (s.charAt(i) >= 'a' && s.charAt(i) <= 'z') {
+                char res = (char) (s.charAt(i) - 32);
+                chaine.append(res);
+            } else {
+                chaine.append(s.charAt(i));
+            }
+        }
+        return chaine.toString();
     }
 }
