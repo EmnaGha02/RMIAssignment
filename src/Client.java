31dfcc0c4e3248e6b097f4307020354fcf3417e9
Emna Gharbi <76482966+EmnaGha02@users.noreply.github.com>
2021-12-12 23:04:56 +0100

    Update Client.java

--- src/Client.java
+++ src/Client.java
@@ -5,6 +5,21 @@ public class Client {
     public static void main(String[] args) {
         try {
            MyInterface service = (MyInterface) Naming.lookup("rmi://127.0.0.1:1250/server");
+            Scanner input = new Scanner(System.in);
+            
+            System.out.println("Donner la premier chaine de caractére à inverser ");
+            String s = input.nextLine();
+            System.out.println(service.reverse(s));
+            
+            System.out.println("Donner la deuxieme chaine de caractére : ");
+            s= input.nextLine();
+            System.out.println(service.caseChanger(s));
+            
+            System.out.println("Donner la troisieme chaine de caractére: ");
+            s= input.nextLine();
+            System.out.println(service.min(s));
+            
+            obj.close();
 
         } catch (Exception e) {
             e.printStackTrace();
